0981542d9110e6baaef111138964719f689d5cac
HoffsMH <hecker.matthew@gmail.com>
2016-01-05 14:04:10 -0700

    made test server and  dev server listen on different ports in server test file.

--- test/server-test.js
+++ test/server-test.js
@@ -1,14 +1,26 @@
 const assert = require('assert');
-const app = require('../server');
+const app    = require('../server');
 
-const _     = require("lodash");
-const chai = require('chai');
+const _      = require("lodash");
+const chai   = require('chai');
 
 const expect = chai.expect;
 
 describe('Server', () => {
 
+  before(done => {
+    this.port = 9876;
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
   it('should exist', () => {
-    expect(app).to.not.be(null)
+    expect(app).to.not.be.undefined
   });
 });
